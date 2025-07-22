932bd6a515f11e6a4e4e17a42f978965b2ce408a
C. Titus Brown <titus@idyll.org>
2025-07-22 13:52:40 -0400

    output tree

--- combine-trees.py
+++ combine-trees.py
@@ -36,24 +36,20 @@ def main():
 
     print(f"loaded {len(names_to_nodes)} distinct names across {total_nodes} nodes in {len(treelist)} trees.")
 
-    # make copy of first tree...
-    first_tree_copy = tree_utils.copy_tree(treelist[0])
-
+    # build an augmented tree that contains all the nodes
     aug_tree = tree_utils.augment_tree(treelist[0], treelist[1:])
     aug_nodes = tree_utils.collect_all_nodes(aug_tree)
 
-    # check that all names are in augmented tree
-    found = set()
-    for node in aug_nodes:
-        found.add(node["name"])
-    missing = set(names_to_nodes) - found
-    assert not missing, len(missing)
+    # we can now adjust the nodes in the aug_tree however we want:
+    for adj_node in aug_nodes:
+        name = adj_node["name"]
+        orig_nodes = names_to_nodes[name]
+        mean_count = sum([ n["count"] for n in orig_nodes ]) / len(orig_nodes)
+        adj_node["count"] = mean_count
 
-    # check that we didn't change initial tree...
-    first_tree = checks.collect_all_nodes(treelist[0])
-    assert checks.trees_are_equal(first_tree, first_tree_copy)
-
-    checks.check_structure(treelist[0])
+    with open(args.output_json, "w") as fp:
+        json.dump(aug_tree, fp)
+    print(f"saved {len(aug_nodes)} nodes to '{args.output_json}'")
 
 
 if __name__ == '__main__':
